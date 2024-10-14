cc49b503c69cee8813a2804ab5e570674c0c9a29
Marcella Fiori de Oliveira <134170324+Miohri@users.noreply.github.com>
2024-10-14 08:56:42 -0300

    Update desafio1-pokemon-matrizes.js

--- desafio1-pokemon-matrizes.js
+++ desafio1-pokemon-matrizes.js
@@ -1,9 +1,9 @@
-let pokemons = [
+let pokemons = {
   ["Poochyena", 2, "M", 13],
   ["Zigzagoon", 2, "F", 13],
   ["Dragonite", 5, "M", 25],
   ["Wurmple", 2, "M", 14]
-]
+  };
 
 console.log(pokemons[0][0] + " cadastrado com sucesso!")
 console.log(pokemons[1][0] + " cadastrado com sucesso!")
