3c7c6e1eb24c84a2ae949dc1366015bea32d9b47
Miohri <marcellafiori23@gmail.com>
2024-11-18 10:37:41 -0300

    Melhorias Desafio 3

--- desafio3.js
+++ desafio3.js
@@ -1,6 +1,6 @@
 // Escrevendo as classes de um Jogo
 
-class hero {
+class Hero {
     constructor(nome, idade, tipo) {
         this.nome = nome;
         this.idade = idade;
@@ -8,34 +8,36 @@ class hero {
     }
     
     atacar() {
-    let ataque = "";
+        let ataque = "";
 
-switch (this.tipo) {
-    case "mago":
-        ataque = "magia";
-        break;
-    case "guerreiro":
-        ataque = "espada";
-        break;
-    case "monge":
-        ataque = "artes marciais";
-        break;
-    case "ninja":
-        ataque = "shuriken";
-        break;
-}
+        switch (this.tipo) {
+            case "mago":
+                ataque = "magia";
+                break;
+            case "guerreiro":
+                ataque = "espada";
+                break;
+            case "monge":
+                ataque = "artes marciais";
+                break;
+            case "ninja":
+                ataque = "shuriken";
+                break;
+            default:
+                ataque = "ataque desconhecido";    
+        }
 
-    console.log(`O ${this.tipo} atacou usando ${ataque}`)
+        console.log(`O ${this.tipo} atacou usando ${ataque}`)
     } 
 
 }
 
-let guerreiro = new hero("Garen", 32, "guerreiro");
-let mago = new hero("Lux", 23, "mago");
-let monge = new hero("Lee Sin", 40, "monge");
-let ninja = new hero ("Akali", 25, "ninja");
+let guerreiro = new Hero("Garen", 32, "guerreiro");
+let mago = new Hero("Lux", 23, "mago");
+let monge = new Hero("Lee Sin", 40, "monge");
+let ninja = new Hero ("Akali", 25, "ninja");
 
-guerreiro.atacar()
-mago.atacar()
-monge.atacar()
-ninja.atacar()
+guerreiro.atacar();
+mago.atacar();
+monge.atacar();
+ninja.atacar();
